bc448ba4653b0df9a1bf8d905951fbe1ab2d264e
Sachico <s104103045@gmail.com>
2025-07-19 14:53:48 +0800

    反引號結合三元運算

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className={false ? "a c" : "b c"}>
+    <div className={`c ${false ? "a" : "b"}`}>
       {true ? <h1>hello</h1> : <h1>world</h1>}
     </div>
   );
