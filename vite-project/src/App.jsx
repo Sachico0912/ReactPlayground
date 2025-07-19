f6619fd67f61f4c0536f6c805315196c25bad07d
Sachico <s104103045@gmail.com>
2025-07-19 14:47:04 +0800

    在className裡面使用三元運算，給元素切換不同CSS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="my-div">{true ? <h1>hello</h1> : <h1>world</h1>}</div>;
+  return (
+    <div className={false ? "a" : "b"}>
+      {true ? <h1>hello</h1> : <h1>world</h1>}
+    </div>
+  );
 }
 
 export default App;
