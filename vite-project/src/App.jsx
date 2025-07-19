d42e6c056a0dcc5dd83cc0a5679b295101bcd673
Sachico <s104103045@gmail.com>
2025-07-19 15:04:20 +0800

    && 邏輯運算

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div className={`c ${false ? "a" : "b"}`}>
-      {true ? <h1>hello</h1> : <h1>world</h1>}
-    </div>
+    <div className={`c ${false ? "a" : "b"}`}>{true && <h1>hello</h1>}</div>
   );
 }
 
 export default App;
+
+// AND 邏輯運算的左邊的值是真值的時候，會回傳右邊的值；反之則回傳左邊
+// JSX 會忽略false 跟 undefined 的值
