0561db1bc11b3c8079102d7ca11b8ea1a4e98759
Sachico <s104103045@gmail.com>
2025-07-19 13:04:36 +0800

    first-commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+function MyComponent() {
+  return <h1>你好</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
+
+// JSX => Vite 裡的編譯工具 => JavaScript
+// 執行時 vite 將 JSX 轉譯成 JS 物件 React 轉換物件為Html
+// PascalCase 命名，每個單字首字母大寫
+// parent component 及 child component
---
+++ vite-project/src/ChildComponent.jsx
@@ -0,0 +1,5 @@
+function ChildComponent() {
+  return <h2>我是子組件</h2>;
+}
+
+export default ChildComponent;
