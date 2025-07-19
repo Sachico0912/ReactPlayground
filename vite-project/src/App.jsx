1f7264a295d12358f36a001101534ab8cf62f61a
Sachico <s104103045@gmail.com>
2025-07-19 16:04:41 +0800

    useState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 function MyComponent() {
-  let clicks = 0;
+  const [clicks, setClicks] = useState(0);
   const handleClick = () => {
-    clicks++;
+    setClicks(clicks + 1);
     console.log(clicks);
   };
   return (
@@ -20,3 +22,8 @@ function App() {
 }
 
 export default App;
+
+// state 是React的核心概念之一，用於在組件中存儲和管理狀態。
+// useState 會回傳陣列
+//1. state 的內容
+//2. 更改 state 內容的函數
