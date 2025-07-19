87374b5ac6464affd6fbc5bbfd64ffead7ffa887
Sachico <s104103045@gmail.com>
2025-07-19 15:32:16 +0800

    解構賦值

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,9 @@
-function MyComponent(props) {
-  console.log(props);
-
-  //{a: 'hello world'}
-  //a: "hello world"
-  //[[Prototype]]: Object
-
-  props.c(); //呼叫父傳遞過來的函式
-
+function MyComponent({ a, b, c }) {
+  c();
   return (
     <>
-      <div>{props.a}</div>
-      <div>{props.b}</div>
+      <div>{a}</div>
+      <div>{b}</div>
     </>
   );
 }
@@ -31,6 +24,5 @@ function App() {
 
 export default App;
 
-// MyComponent 是子
-// App 是父
-// props 是父傳遞資料給子
+// 物件的解構賦值是以屬性名來取用資料
+// 順序不重要，名字很重要
